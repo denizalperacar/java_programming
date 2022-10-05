dc7b0776c8acc57b81f60f58e7cd25b714ea0cbe
deniz <denizalperacar@gmail.com>
2022-10-05 23:53:04 +0300

    week 5 HW done

--- coursera/cs_prinston/TowerOfHonai.java
+++ coursera/cs_prinston/TowerOfHonai.java
@@ -1,5 +1,18 @@
 public class TowerOfHonai {
     
-    
+    public static String move(int n, boolean left) {
+
+        if (n == 0) return " ";
+        if (left) return n + "L";
+        else
+
+
+    } 
+
+    public static void main(String[] args) {
+
+
+
+    }
 
 }

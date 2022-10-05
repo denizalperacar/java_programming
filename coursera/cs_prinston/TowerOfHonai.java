b827de469660a6911df99aebe1a2dfc7e7a8065a
deniz <denizalperacar@gmail.com>
2022-10-06 00:00:04 +0300

    implemeted the tower of honai algorithm

--- coursera/cs_prinston/TowerOfHonai.java
+++ coursera/cs_prinston/TowerOfHonai.java
@@ -1,18 +1,15 @@
 public class TowerOfHonai {
     
     public static String move(int n, boolean left) {
-
+        String m;
         if (n == 0) return " ";
-        if (left) return n + "L";
-        else
-
-
+        if (left) m = n + "L";
+        else m =  n + "R";
+        return move(n-1, !left) + m + move(n-1, !left); 
     } 
 
     public static void main(String[] args) {
-
-
-
+        int n = Integer.parseInt(args[0]);
+        StdOut.println(move(n, false));
     }
-
 }
